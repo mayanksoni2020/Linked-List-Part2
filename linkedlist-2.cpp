d743ad9f64abb226b89fe1eadb8235b3ed34c489
mayanksoni2020 <48762957+mayanksoni2020@users.noreply.github.com>
2019-09-28 23:24:25 +0530

    Implemented display feature
    add more feature to explore it more

---
+++ linkedlist-2.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+int data;
+node *link
+};
+class list{
+private:
+node *head,*tail;
+public:
+list()
+{
+head=NULL;
+tail=NULL;
+}
+void insert(int n){
+node *temp=new node();
+temp->data=n;
+temp->link=NULL;
+while(head==NULL){
+head=temp;
+tail=temp;
+}
+tail=temp;
+tail->link=NULL;
+}
+void display()
+{
+node*temp=new node();
+temp=head;
+while(temp!=NULL)
+{
+cout<<temp->data<<endl;
+temp=temp->link
+}
+}
+};
+int main(){
+list obj;
+obj.insert(15);
+obj.display()
+}
